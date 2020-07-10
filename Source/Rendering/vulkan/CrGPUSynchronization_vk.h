99884f15e062c554cff10137019109645bb125c1
Emilio López <redorav@gmail.com>
2020-07-10 10:02:09 +0100

    Remove comments from vk synchronization as they now live in platform-independent code

--- Source/Rendering/vulkan/CrGPUSynchronization_vk.h
+++ Source/Rendering/vulkan/CrGPUSynchronization_vk.h
@@ -5,7 +5,6 @@
 
 class ICrRenderDevice;
 
-// Fences do CPU-GPU synchronization
 class CrGPUFenceVulkan final : public ICrGPUFence
 {
 public:
@@ -28,7 +27,6 @@ inline const VkFence& CrGPUFenceVulkan::GetVkFence() const
 	return m_vkFence;
 }
 
-// Semaphores do inter-queue synchronization
 class CrGPUSemaphoreVulkan final : public ICrGPUSemaphore
 {
 public:
@@ -50,5 +48,3 @@ inline const VkSemaphore& CrGPUSemaphoreVulkan::GetVkSemaphore() const
 {
 	return m_vkSemaphore;
 }
-
-// TODO Create events for fine-grained command buffer sync

ef701ef6617132d70855b3875261b1e0dddff908
Emilio López <redorav@gmail.com>
2021-03-15 00:14:21 +0000

    Add Initialize function to the CPU stack allocator

--- Source/Rendering/CrCPUStackAllocator.h
+++ Source/Rendering/CrCPUStackAllocator.h
@@ -1,13 +1,15 @@
 #pragma once
 
+#include "Core/CrAlignment.h"
+
 // Manages transient memory allocated per frame for CPU resources
 class CrCPUStackAllocator
 {
 public:
 
-	CrCPUStackAllocator();
-
 	~CrCPUStackAllocator();
+
+	void Initialize(uint32_t size);
 
 	template<typename T, typename... Args>
 	T* AllocateObject(Args... args);
@@ -24,6 +26,15 @@ protected:
 
 	size_t m_poolSize = 0;
 };
+
+inline void* CrCPUStackAllocator::Allocate(uint32_t size)
+{
+	void* bufferPointer = m_currentPointer; // Take the current pointer
+	size_t alignedBufferSize = Align256(size); // Align memory as required
+	CrAssertMsg(m_currentPointer + alignedBufferSize < m_memoryBasePointer + m_poolSize, "Ran out of memory in stream");
+	m_currentPointer += alignedBufferSize; // Reserve as many bytes as needed by the buffer
+	return bufferPointer;
+}
 
 template<typename T, typename... Args>
 T* CrCPUStackAllocator::AllocateObject(Args... args)

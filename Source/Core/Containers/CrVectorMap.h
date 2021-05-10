73a72419a5526de09f7b403f0b4cc0db8b87b9c0
Emilio López <redorav@gmail.com>
2021-05-10 23:06:21 +0100

    Add VectorMap and VectorSet

---
+++ Source/Core/Containers/CrVectorMap.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <EASTL/vector_map.h>
+
+template<typename K, typename T>
+using CrVectorMap = eastl::vector_map<K, T>;
---
+++ Source/Core/Containers/CrVectorSet.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <EASTL/vector_set.h>
+
+template<typename T>
+using CrVectorSet = eastl::vector_set<T>;

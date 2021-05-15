85efbe694a6549b684d20304f95eeef7265c22a8
Emilio López <redorav@gmail.com>
2021-05-15 22:27:46 +0100

    Make sure to disable reallocation on fixed strings

--- Source/Core/String/CrFixedString.h
+++ Source/Core/String/CrFixedString.h
@@ -4,24 +4,25 @@
 
 // Take care to take the null terminator into account, i.e. a fixed string 
 // of 16 has 15 usable characters
-using CrFixedString16 = eastl::fixed_string<char, 16>;
-using CrFixedString32 = eastl::fixed_string<char, 32>;
-using CrFixedString64 = eastl::fixed_string<char, 64>;
-using CrFixedString128 = eastl::fixed_string<char, 128>;
-using CrFixedString256 = eastl::fixed_string<char, 256>;
-using CrFixedString512 = eastl::fixed_string<char, 512>;
-using CrFixedString1024 = eastl::fixed_string<char, 1024>;
 
 template<int N>
-using CrFixedString = eastl::fixed_string<char, N>;
+using CrFixedString = eastl::fixed_string<char, N, false>;
 
-using CrFixedWString16 = eastl::fixed_string<wchar_t, 16>;
-using CrFixedWString32 = eastl::fixed_string<wchar_t, 32>;
-using CrFixedWString64 = eastl::fixed_string<wchar_t, 64>;
-using CrFixedWString128 = eastl::fixed_string<wchar_t, 128>;
-using CrFixedWString256 = eastl::fixed_string<wchar_t, 256>;
-using CrFixedWString512 = eastl::fixed_string<wchar_t, 512>;
-using CrFixedWString1024 = eastl::fixed_string<wchar_t, 1024>;
+using CrFixedString16 = eastl::fixed_string<char, 16, false>;
+using CrFixedString32 = eastl::fixed_string<char, 32, false>;
+using CrFixedString64 = eastl::fixed_string<char, 64, false>;
+using CrFixedString128 = eastl::fixed_string<char, 128, false>;
+using CrFixedString256 = eastl::fixed_string<char, 256, false>;
+using CrFixedString512 = eastl::fixed_string<char, 512, false>;
+using CrFixedString1024 = eastl::fixed_string<char, 1024, false>;
 
 template<int N>
-using CrFixedWString = eastl::fixed_string<wchar_t, N>;
+using CrFixedWString = eastl::fixed_string<wchar_t, N, false>;
+
+using CrFixedWString16 = eastl::fixed_string<wchar_t, 16, false>;
+using CrFixedWString32 = eastl::fixed_string<wchar_t, 32, false>;
+using CrFixedWString64 = eastl::fixed_string<wchar_t, 64, false>;
+using CrFixedWString128 = eastl::fixed_string<wchar_t, 128, false>;
+using CrFixedWString256 = eastl::fixed_string<wchar_t, 256, false>;
+using CrFixedWString512 = eastl::fixed_string<wchar_t, 512, false>;
+using CrFixedWString1024 = eastl::fixed_string<wchar_t, 1024, false>;

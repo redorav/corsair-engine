a5c330db1d75637cc25cd95db192400797d8069c
Emilio López <redorav@gmail.com>
2021-08-02 19:36:22 +0100

    Increase size of the directory iterator function

--- Source/Core/FileSystem/ICrFile.h
+++ Source/Core/FileSystem/ICrFile.h
@@ -17,7 +17,7 @@ struct CrDirectoryEntry
 	bool isDirectory = false;
 };
 
-using FileIteratorFn = CrFixedFunction<4, bool(const CrDirectoryEntry& entry)>;
+using FileIteratorFn = CrFixedFunction<12, bool(const CrDirectoryEntry& entry)>;
 
 namespace FileOpenFlags
 {

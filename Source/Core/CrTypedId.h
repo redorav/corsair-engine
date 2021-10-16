4c2da1c04376bcf9aeb1b28603c109af8b7107df
Emilio López <redorav@gmail.com>
2021-10-16 22:29:37 +0100

    Add CrTypedId, a type-safe identifier
    This will help not mix e.g. visibility and instance id together, avoiding unnecessary bugs

---
+++ Source/Core/CrTypedId.h
@@ -0,0 +1,25 @@
+#pragma once
+
+template<typename T, typename U>
+class CrTypedId
+{
+public:
+
+	CrTypedId() : id(0) {}
+	explicit CrTypedId(U id) : id(id) {}
+
+	CrTypedId& operator = (U otherId) { id = otherId; return *this; }
+
+	CrTypedId operator - (CrTypedId otherId) { return CrTypedId(id - otherId.id); }
+	CrTypedId operator - (U otherId) { return CrTypedId(id - otherId); }
+
+	bool operator < (CrTypedId otherId) { return id < otherId.id; }
+	bool operator > (CrTypedId otherId) { return id > otherId.id; }
+	bool operator == (CrTypedId otherId) { return id == otherId.id; }
+	bool operator != (CrTypedId otherId) { return id != otherId.id; }
+
+	CrTypedId& operator ++ () { id++; return *this; } // Pre-increment
+	CrTypedId operator ++ (int) { CrTypedId temp = *this; id++; return temp; } // Post-increment
+
+	U id;
+};

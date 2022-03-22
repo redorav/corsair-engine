72b63cc55ffa181c6b3f4971f1ae3e43311c5d87
Emilio López <redorav@gmail.com>
2022-03-22 23:34:28 +0000

    Commit pipeline header

--- Source/Rendering/d3d12/CrPipeline_d3d12.h
+++ Source/Rendering/d3d12/CrPipeline_d3d12.h
@@ -14,6 +14,13 @@ public:
 		const ICrGraphicsShader* graphicsShader, const CrVertexDescriptor& vertexDescriptor
 	);
 
+	ID3D12PipelineState* GetD3D12PipelineState() const { return m_d3d12PipelineState; }
+	D3D12_PRIMITIVE_TOPOLOGY GetD3D12PrimitiveTopologyType() const { return m_d3d12PrimitiveTopology; }
+
+private:
+
+	D3D12_PRIMITIVE_TOPOLOGY m_d3d12PrimitiveTopology;
+
 	ID3D12PipelineState* m_d3d12PipelineState;
 };
 
@@ -26,5 +33,6 @@ public:
 		const CrRenderDeviceD3D12* d3d12RenderDevice, const ICrComputeShader* computeShader
 	);
 
+	ID3D12PipelineState* GetD3D12PipelineState() const { return m_d3d12PipelineState; }
 	ID3D12PipelineState* m_d3d12PipelineState;
 };

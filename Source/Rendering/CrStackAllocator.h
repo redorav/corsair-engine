2c138cdc7058880ea642549853b579b043c60b4b
Emilio López <redorav@gmail.com>
2022-06-04 16:28:14 +0200

    Rework stack allocator to accept templated allocations

--- Source/Rendering/CrStackAllocator.h
+++ Source/Rendering/CrStackAllocator.h
@@ -16,11 +16,16 @@ class CrStackAllocator
 {
 public:
 
-	CrStackAllocation<void> Allocate(size_t size);
+	CrStackAllocation<void> Allocate(size_t sizeBytes);
+
+	template<typename T>
+	CrStackAllocation<T> Allocate(size_t count);
 
 	CrStackAllocation<void> AllocateAligned(size_t size, size_t alignment);
 
 protected:
+
+	void AllocateAligned(size_t size, size_t alignment, void*& memory, uint32_t& offset);
 
 	uint8_t* m_memoryBasePointer = nullptr;
 
@@ -29,16 +34,32 @@ protected:
 	size_t m_size = 0;
 };
 
-inline CrStackAllocation<void> CrStackAllocator::Allocate(size_t size)
+inline CrStackAllocation<void> CrStackAllocator::Allocate(size_t sizeBytes)
 {
-	return AllocateAligned(size, 1);
+	return AllocateAligned(sizeBytes, 1);
 }
 
-inline CrStackAllocation<void> CrStackAllocator::AllocateAligned(size_t size, size_t alignment)
+template<typename T>
+inline CrStackAllocation<T> CrStackAllocator::Allocate(size_t count)
+{
+	void* bufferPointer; uint32_t offset;
+	AllocateAligned(sizeof(T) * count, 1, bufferPointer, offset);
+	return CrStackAllocation<T>((T*)bufferPointer, offset);
+}
+
+inline CrStackAllocation<void> CrStackAllocator::AllocateAligned(size_t sizeBytes, size_t alignment)
+{
+	void* bufferPointer; uint32_t offset;
+	AllocateAligned(sizeBytes, alignment, bufferPointer, offset);
+	return CrStackAllocation<void>(bufferPointer, offset);
+}
+
+inline void CrStackAllocator::AllocateAligned(size_t sizeBytes, size_t alignment, void*& memory, uint32_t& offset)
 {
 	uint8_t* bufferPointer = CrAlignUpPow2(m_currentPointer, alignment); // Copy the current pointer, with the correct alignment
-	m_currentPointer = bufferPointer + size; // Reserve as many bytes as needed by the buffer
+	m_currentPointer = bufferPointer + sizeBytes; // Reserve as many bytes as needed by the buffer
 	CrAssertMsg(m_currentPointer < m_memoryBasePointer + m_size, "Ran out of memory in stream");
-	uint32_t offset = static_cast<uint32_t>(bufferPointer - m_memoryBasePointer); // Work out the offset
-	return CrStackAllocation<void>(bufferPointer, offset);
+
+	memory = bufferPointer;
+	offset = static_cast<uint32_t>(bufferPointer - m_memoryBasePointer); // Work out the offset
 }

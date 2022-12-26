d53b9bdbc0ee2a7ba95fb4da55e2612179d1d745
Emilio López <redorav@gmail.com>
2022-12-26 20:05:05 +0000

    Add an initial implementation of atomic and intrusive_ptr
    Still a ways to go, but it's the first foray into replacing shared_ptr with a more lightweight implementation of the smart pointer
    The advantages are that there are fewer allocations, we can create an intrusive pointer from this, and the deleter is implicit in the base class by inheritance, no need to carry the deleter around
    The atomic class will also allow us to do more involved synchronization operations
    One of the reasons for replacing EASTL, at least partly, is to avoid long compile times on many of its classes which bring in too much functionality, and to learn new things :)

---
+++ Source/Core/CRSTL/intrusive_ptr.h
@@ -0,0 +1,127 @@
+#pragma once
+
+#include "atomic.h"
+
+namespace crstl
+{
+	template<typename T>
+	void intrusive_ptr_add_ref(T* ptr)
+	{
+		ptr->add_ref();
+	}
+
+	template<typename T>
+	void intrusive_ptr_release(T* ptr)
+	{
+		int32_t currentRef = ptr->release_ref();
+
+		if (currentRef == 0)
+		{
+			ptr->delete_callback();
+		}
+	}
+
+	template<typename T>
+	class intrusive_ptr
+	{
+	public:
+
+		intrusive_ptr() = default;
+
+		intrusive_ptr(T* ptr) : m_ptr(ptr)
+		{
+			if (ptr)
+			{
+				intrusive_ptr_add_ref(ptr);
+			}
+		}
+
+		~intrusive_ptr()
+		{
+			if (m_ptr)
+			{
+				intrusive_ptr_release(m_ptr);
+			}
+		}
+
+		intrusive_ptr& operator = (T* ptr)
+		{
+			if (m_ptr != ptr)
+			{
+				T* const ptr_temp = m_ptr;
+
+				// Add a reference to the new pointer
+				if (ptr)
+				{
+					intrusive_ptr_add_ref(ptr);
+				}
+
+				// Assign to member pointer
+				m_ptr = ptr;
+
+				// Release reference from the old pointer we used to hold on to
+				if (ptr_temp)
+				{
+					intrusive_ptr_release(ptr_temp);
+				}
+			}
+
+			return *this;
+		}
+
+		T* get() const { return m_ptr; }
+
+		T* operator ->() const
+		{
+			return m_ptr;
+		}
+
+		typedef T* (intrusive_ptr<T>::*boolean)() const;
+
+		operator boolean() const
+		{
+			// Return anything that isn't easily castable but is guaranteed to be non-null, such as the get function pointer
+			return m_ptr ? &intrusive_ptr<T>::get : nullptr;
+		}
+
+		bool operator!() const
+		{
+			return (m_ptr == nullptr);
+		}
+
+	private:
+
+		T* m_ptr = nullptr;
+	};
+
+	template<typename T>
+	class intrusive_interface
+	{
+	public:
+
+		virtual int32_t add_ref()
+		{
+			m_refcount++;
+			return m_refcount;
+		}
+
+		virtual int32_t release_ref()
+		{
+			m_refcount--;
+
+			if (m_refcount == 0)
+			{
+				// Delete the thing
+			}
+
+			return m_refcount;
+		}
+
+		virtual void delete_callback()
+		{
+			delete this;
+		}
+
+		crstl::atomic<int32_t> m_refcount;
+	};
+};

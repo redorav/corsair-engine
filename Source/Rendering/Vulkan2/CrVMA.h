fc1cb7cb0e31c066ab708a845e2a50bbf9e7887c
Emilio López <redorav@gmail.com>
2022-12-30 22:22:57 +0000

    Change folder name to change case

--- Source/Rendering/Vulkan2/CrVMA.h
+++ Source/Rendering/Vulkan2/CrVMA.h
@@ -1,7 +1,11 @@
 // Don't add pragma once here. It needs to be included more than once for the implementation
 
 #include "Core/CrMacros.h"
+#include "Core/Logging/ICrDebug.h"
 
 warnings_off
+
+#define VMA_DEBUG_LOG(format, ...) CrLog(format, __VA_ARGS__)
+
 #include <vk_mem_alloc.h>
 warnings_on

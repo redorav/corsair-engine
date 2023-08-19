50378239477b6b6cd524f84195ebc9f0ead7598d
Emilio López <redorav@gmail.com>
2023-08-19 21:11:48 +0100

    Add grey

--- Source/Rendering/Shaders/Colors.hlsl
+++ Source/Rendering/Shaders/Colors.hlsl
@@ -11,6 +11,7 @@ namespace Colors
 	static const float3 Yellow  = float3(1.0, 1.0, 0.0);
 	
 	static const float3 White   = float3(1.0, 1.0, 1.0);
+	static const float3 Grey    = float3(0.5, 0.5, 0.5);
 	static const float3 Black   = float3(0.0, 0.0, 0.0);
 };
 

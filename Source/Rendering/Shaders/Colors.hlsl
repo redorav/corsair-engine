41f400c08364961b1e24327cc735c206f5104480
Emilio López <redorav@gmail.com>
2023-08-19 13:18:37 +0100

    Add Colors.hlsl
    Just a collection of useful colors

---
+++ Source/Rendering/Shaders/Colors.hlsl
@@ -0,0 +1,17 @@
+#ifndef COLORS_HLSL
+#define COLORS_HLSL
+
+namespace Colors
+{
+	static const float3 Red     = float3(1.0, 0.0, 0.0);
+	static const float3 Green   = float3(0.0, 1.0, 0.0);
+	static const float3 Blue    = float3(0.0, 0.0, 1.0);
+	static const float3 Cyan    = float3(0.0, 1.0, 1.0);
+	static const float3 Magenta = float3(1.0, 0.0, 1.0);
+	static const float3 Yellow  = float3(1.0, 1.0, 0.0);
+	
+	static const float3 White   = float3(1.0, 1.0, 1.0);
+	static const float3 Black   = float3(0.0, 0.0, 0.0);
+};
+
+#endif

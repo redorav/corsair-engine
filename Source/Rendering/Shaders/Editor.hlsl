f2c1949372c0b867de56686e4547e7ac00f0bf0f
Emilio López <redorav@gmail.com>
2023-08-19 22:32:03 +0100

    Add first version of the grid shader
    Some things still pending, like the subdivision count, aliasing at the horizon
    Would like to have two levels of grid as well

--- Source/Rendering/Shaders/Editor.hlsl
+++ Source/Rendering/Shaders/Editor.hlsl
@@ -4,6 +4,7 @@
 #include "Common.hlsl"
 #include "ComputeCommon.hlsl"
 #include "GBuffer.hlsl"
+#include "Colors.hlsl"
 
 Texture2D EditorSelectionTexture;
 
@@ -33,6 +34,107 @@ float4 EditorEdgeSelectionPS(VSOutputFullscreen psInput) : SV_Target0
 	{
 		return float4(0.0, 0.0, 0.0, 0.0);
 	}
+}
+
+struct EditorGridCB
+{
+	float4 gridParams; // .x scale .y subdivisions
+};
+
+cbuffer EditorGridCB
+{
+	EditorGridCB cb_EditorGrid;
+};
+
+struct VSOutputEditorGrid
+{
+	float4 hwPosition    : SV_Position;
+	float4 worldPosition : WORLD_POSITION;
+	float4 viewPosition  : VIEW_POSITION;
+	float4 color         : COLOR0; // Color of the grid, plus alpha for fading
+	float4 gridUV        : TEXCOORD0;
+};
+
+VSOutputEditorGrid EditorGridVS(uint vertexId : SV_VertexID)
+{
+	VSOutputEditorGrid vsOutput = (VSOutputEditorGrid)0;
+	
+	float3 positionLocal = 0.0;
+	float3 color = 0.0;
+	float2 gridUV = 0.0;
+	
+	switch (vertexId)
+	{
+		case 0:
+			positionLocal = float3(-1, 0, 1);
+			color = Colors::Red;
+			gridUV = float2(0.0, 1.0);
+			break;
+		case 1:
+			positionLocal = float3(1, 0, 1);
+			color = Colors::Green;
+			gridUV = float2(1.0, 1.0);
+			break;
+		case 2:
+			positionLocal = float3(1, 0, -1);
+			color = Colors::Blue;
+			gridUV = float2(1.0, 0.0);
+			break;
+		case 3:
+			positionLocal = float3(-1, 0, 1);
+			color = Colors::Magenta;
+			gridUV = float2(0.0, 1.0);
+			break;
+		case 4:
+			positionLocal = float3(1, 0, -1);
+			color = Colors::Cyan;
+			gridUV = float2(1.0, 0.0);
+			break;
+		case 5:
+			positionLocal = float3(-1, 0, -1);
+			color = Colors::Yellow;
+			gridUV = float2(0.0, 0.0);
+			break;
+	}
+	
+	float4 positionWorld = float4(positionLocal.xyz * cb_EditorGrid.gridParams.x, 1);
+
+	float4 positionView = mul(positionWorld, cb_Camera.world2View);
+	
+	float4 positionHomogeneous = mul(positionView, cb_Camera.view2Projection);
+
+	vsOutput.hwPosition = positionHomogeneous;
+	vsOutput.worldPosition = positionWorld;
+	vsOutput.viewPosition = positionView;
+	vsOutput.color.rgb = color;
+	vsOutput.gridUV.xy = gridUV;
+
+	return vsOutput;
+}
+
+// Shader used to render the editor grid. We procedurally create the grid via constants
+// and the grid itself will be alpha blended with the rest of the scene
+// A wonderful explanation on how to create a procedural grid: https://madebyevan.com/shaders/grid/
+float4 EditorGridPS(VSOutputEditorGrid psInput) : SV_Target0
+{
+	float2 coord = psInput.worldPosition.xz / cb_EditorGrid.gridParams.y;
+	
+	float horizontalDistanceToCamera = length(cb_Camera.worldPosition.xz - psInput.worldPosition.xz);
+	
+	float2 grid = abs(frac(coord - 0.5) - 0.5) / fwidth(coord);
+	
+	float gridLine = min(grid.x, grid.y);
+
+	float alpha = 1.0 - min(gridLine, 1.0);
+
+	float alphaFade = 1.0 - saturate(horizontalDistanceToCamera / 400.0);
+	
+	alpha *= pow(alphaFade, 8.0);
+	
+	// Gamma correction
+	alpha = pow(alpha, 1.0 / 2.2);
+	
+	return float4(Colors::White, alpha);
 }
 
 Texture2D EditorInstanceIDTexture;

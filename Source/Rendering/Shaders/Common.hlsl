06aaa817485c38f8ea7bcbd3fa3f3b8f38dea401
Emilio López <redorav@gmail.com>
2024-10-26 09:48:32 +0100

    Update dxc compiler

--- Source/Rendering/Shaders/Common.hlsl
+++ Source/Rendering/Shaders/Common.hlsl
@@ -154,7 +154,7 @@ float3 sRGBToLinear(float3 xSRGB)
 float2 OctahedralEncode(float3 n)
 {
 	n /= (abs(n.x) + abs(n.y) + abs(n.z));
-	n.xy = n.z >= 0.0 ? n.xy : ((1.0 - abs(n.yx)) * (n.xy >= 0.0 ? 1.0 : -1.0));
+	n.xy = select(n.z >= 0.0, n.xy, ((1.0 - abs(n.yx)) * select(n.xy >= 0.0, 1.0, -1.0)));
 	return n.xy * 0.5 + 0.5;
 }
 
@@ -164,7 +164,7 @@ float3 OctahedralDecode(float2 f)
 	f = f * 2.0 - 1.0;
 	float3 n = float3(f.x, f.y, 1.0 - abs(f.x) - abs(f.y));
 	float t = saturate(-n.z);
-	n.xy += n.xy >= 0.0 ? -t : t;
+	n.xy += select(n.xy >= 0.0, -t, t);
 	return normalize(n);
 }
 

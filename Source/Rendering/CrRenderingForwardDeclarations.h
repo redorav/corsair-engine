b41346f62882f0b0073ea316df3271b9ccfb654d
Emilio López <redorav@gmail.com>
2024-11-10 20:05:28 +0000

    Add typed buffers to forward declarations

--- Source/Rendering/CrRenderingForwardDeclarations.h
+++ Source/Rendering/CrRenderingForwardDeclarations.h
@@ -59,6 +59,7 @@ namespace Textures { enum T : uint8_t; }
 namespace RWTextures { enum T : uint8_t; }
 namespace StorageBuffers { enum T : uint8_t; }
 namespace RWStorageBuffers { enum T : uint8_t; }
+namespace TypedBuffers { enum T : uint8_t; }
 namespace RWTypedBuffers { enum T : uint8_t; }
 
 struct ConstantBufferMetadata;

3e8a7ebe2e4275d0f69b66c9e6d32a4357db3409
Emilio López <redorav@gmail.com>
2024-11-12 20:46:48 +0000

    Views must match resources

--- Source/Rendering/Shaders/PostProcessing.hlsl
+++ Source/Rendering/Shaders/PostProcessing.hlsl
@@ -5,12 +5,12 @@
 #include "Common.hlsl"
 
 Texture2D<float3> HDRInput;
-RWTexture2D<float3> RWPostProcessedOutput;
+RWTexture2D<float4> RWPostProcessedOutput;
 
 [numthreads(8, 8, 1)]
 void PostProcessingCS(CSInput csInput)
 {
-	RWPostProcessedOutput[csInput.dispatchThreadId.xy] = LinearToSRGB(HDRInput[csInput.dispatchThreadId.xy]);
+	RWPostProcessedOutput[csInput.dispatchThreadId.xy].xyz = LinearToSRGB(HDRInput[csInput.dispatchThreadId.xy]);
 }
 
 #endif

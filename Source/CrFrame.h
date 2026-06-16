638a4b13de4649f957f477a4f5cb181ab54eaf9f
Emilio López <redorav@gmail.com>
2026-06-16 01:14:43 +0100

    Missed change

--- Source/CrFrame.h
+++ Source/CrFrame.h
@@ -36,7 +36,7 @@ private:
 
 	uint32_t m_currentCommandBuffer = 0;
 
-	crstl::vector<crgfx::CrCommandBufferHandle> m_drawCmdBuffers; // Command buffers used for rendering
+	crstl::vector<crgfx::CommandBufferHandle> m_drawCmdBuffers; // Command buffers used for rendering
 	
 	CrComputePipelineHandle m_exampleComputePipeline;
 
